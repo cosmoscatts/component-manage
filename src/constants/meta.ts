af1dfc7c1f494079a4d5856c87ab76803b257b1e
cosmoscatts <2138889191@qq.com>
2024-03-22 14:59:36 +0800

    chore: init

---
+++ src/constants/meta.ts
@@ -0,0 +1,7 @@
+export const APP_META = {
+  author: 'Cosmoscatts',
+  name: 'Vivid Arco Admin',
+  description: 'Vue3 管理系统（Arco Design）',
+  github: 'https://github.com/cosmoscatts/vivid',
+  copyRight: 'Vivid Arco Admin - Made by Cosmoscatts',
+}
---
+++ src/pages/component-manage/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>组态管理</div>
+</template>
